8905a2340d07b798c36e475acf574f88aa3c2f57
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2025-11-22 19:44:53 +0100

    Enable JSX and add max-lines rule to ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -3,11 +3,13 @@ import antfu from '@antfu/eslint-config';
 export default antfu({
   react: true,
   typescript: true,
+  jsx: true,
   stylistic: {
     indent: 2,
     quotes: 'single',
   },
   rules: {
     'style/semi': ['error', 'always'],
+    'max-lines': ['error', { max: 80 }],
   },
 });
