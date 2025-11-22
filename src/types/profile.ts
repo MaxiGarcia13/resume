1d71cb60752a4cfce69012783b6919d83cd1e703
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2025-11-22 19:42:41 +0100

    Add type definitions for Profile and Variant

---
+++ src/types/profile.ts
@@ -0,0 +1,10 @@
+export interface Profile {
+  name: string
+  nickName: string
+  email: string
+  role: string
+  socialMedia: {
+    linkedin: string
+    github: string
+  }
+}
---
+++ src/types/variants.ts
@@ -0,0 +1,1 @@
+export type Variant = 'ghost' | 'primary';
