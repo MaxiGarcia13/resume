4b84e9d3fdc4658e0cc5426513e3202c8b2e5923
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2025-11-22 19:45:28 +0100

    Add theme switcher and update layout for light/dark mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { Header } from './components';
+import { Experiences, Header, SwitchTheme } from './components';
+import { ThemeProvider } from './context';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Header />
+    <ThemeProvider>
+      <SwitchTheme className="absolute right-4 top-4 w-[40px]" />
+      <Header />
+      <Experiences />
+    </ThemeProvider>
   </StrictMode>,
 );
