caafd35568741e439c04ec35e5f433ea5e21d2d6
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2025-11-23 00:51:26 +0100

    Remove experiences data and update component usage

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { Experiences, Header, SwitchTheme } from './components';
+import { Header, SwitchTheme, WroksExperience } from './components';
 import { ThemeProvider } from './context';
 
 createRoot(document.getElementById('root')!).render(
@@ -8,7 +8,7 @@ createRoot(document.getElementById('root')!).render(
     <ThemeProvider>
       <SwitchTheme className="absolute right-4 top-4 w-[40px]" />
       <Header />
-      <Experiences />
+      <WroksExperience />
     </ThemeProvider>
   </StrictMode>,
 );
