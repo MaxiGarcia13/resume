b680c10c581369665c6bab9fb6c70c7087ab334c
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2025-11-28 15:40:55 +0100

    Add Web Chatbot project and remove Github searcher

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -3,6 +3,12 @@ import type { Project } from '@/types/projects';
 export function getProjects(): Array<Project> {
   return [
     {
+      title: 'Web Chatbot powered by Web LLM',
+      description: 'Chat component integrated with web-llm (React JS)',
+      repo: 'https://github.com/MaxiGarcia13/web-llm-chat',
+      website: 'https://maxi-gpt.vercel.app/',
+    },
+    {
       title: 'Bilde konverterer',
       description: 'Transform images faster and simpler. The frontend is built with React and TypeScript, while the backend is built with Node.js and Express JS.',
       repo: 'https://github.com/MaxiGarcia13/bildekonverterer/tree/main',
@@ -19,12 +25,6 @@ export function getProjects(): Array<Project> {
       description: 'A Practical Guide to React State Management: useState, useEffect, Context, Redux, and Redux Saga.',
       repo: 'https://github.com/MaxiGarcia13/examples-react',
       website: 'https://examples-react.vercel.app/',
-    },
-    {
-      title: 'Github repository searcher',
-      description: 'Search repositories in GitHub, only filter by ID. Built with React and TypeScript.',
-      repo: 'https://github.com/MaxiGarcia13/github-repository-searcher',
-      website: 'https://github-repository-searcher.vercel.app/',
     },
   ];
 }
