e042f9ce26c8508a825fe1ea3a88cd18c67452d9
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2026-05-13 11:36:28 +0200

    refactor: Migrate icon components from React to Astro for improved performance and maintainability

--- src/components/shared/icons/index.ts
+++ src/components/shared/icons/index.ts
@@ -1,8 +1,8 @@
-export * from './download';
-export * from './email';
-export * from './external-link';
-export * from './github';
-export * from './link';
-export * from './linkedin';
-export * from './moon';
-export * from './sun';
+export { default as DownloadIcon } from './download.astro';
+export { default as EmailIcon } from './email.astro';
+export { default as ExternalLinkIcon } from './external-link.astro';
+export { default as GithubIcon } from './github.astro';
+export { default as LinkIcon } from './link.astro';
+export { default as LinkedInIcon } from './linkedin.astro';
+export { default as MoonIcon } from './moon.astro';
+export { default as SunIcon } from './sun.astro';
