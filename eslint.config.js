0e51c4f7367b659dc21a9b966cd676d8d13ea1b6
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2026-05-13 11:32:55 +0200

    refactor: Remove React integration and update ESLint configuration to include CSS formatter; clean up package dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,10 @@
 import { eslintConfig } from '@maxigarcia/eslint-config';
 
 export default eslintConfig({
-  react: true,
   typescript: true,
-  jsx: true,
   astro: true,
+
+  formatters: {
+    css: true,
+  },
 });
