0632d1704adae56753e26107234936e8b8ace00b
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2026-05-14 17:00:38 +0200

    feat: Add ProjectPreview component for enhanced project display and remove Iframe component

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,7 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  image: {
+    domains: ['snap-website-api.vercel.app'],
+  },
 });
--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -1,7 +1,6 @@
 export * from './actions';
 export * from './heading';
 export * from './icons';
-export { default as Iframe } from './iframe.astro';
 export { default as Img } from './img.astro';
 export { default as Skeleton } from './skeleton.astro';
 export * from './text';
