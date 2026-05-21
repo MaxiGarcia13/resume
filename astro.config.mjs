76b0ffd093ab48018a9dac14862de03b165e8996
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2026-05-21 13:37:11 +0200

    feat: Integrate sitemap support and update site configuration in Astro project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
+
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   image: {
     domains: ['snap-website-api.vercel.app'],
   },
+  site: 'https://maxi-garcia-mortigliengo-cv.vercel.app/',
+  integrations: [sitemap()],
 });
