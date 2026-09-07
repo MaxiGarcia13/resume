5958851d2f7fa88ccc5a986728d2b843c3715fbd
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2026-09-07 10:46:39 +0200

    fix: update z-index in Aside component for improved layering and visibility

--- src/components/ai/aside.tsx
+++ src/components/ai/aside.tsx
@@ -43,7 +43,7 @@ export function Aside({ onClose, ...props }: AsideProps) {
       ref={asideRef}
       className={
         cn(
-          'overflow-hidden fixed flex flex-col z-10',
+          'overflow-hidden fixed flex flex-col z-20',
           'right-0 top-0 sm:right-6 sm:top-16',
           'min-w-64 sm:max-w-96 w-screen h-screen sm:h-[calc(100%-64px-24px)]',
           'border border-gray-200 bg-gray-50 dark:border-gray-700 dark:bg-gray-800',
